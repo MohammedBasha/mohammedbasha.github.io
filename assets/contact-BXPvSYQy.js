f298b5f69c7067c43ef701a1bd17a778c34d7c72
MohammedBasha <me@mohammedbasha.com>
2026-03-25 01:04:32 +0200

    Enhancements

--- assets/contact-BXPvSYQy.js
+++ assets/contact-BXPvSYQy.js
@@ -1,1 +1,8 @@
-var a={email:"mohammedbasha9191@gmail.com",phone:"+201115793403",whatsapp:"https://wa.me/201115793403",linkedin:"https://linkedin.com/in/mohammedbasha",github:"https://github.com/mohammedbasha"};export{a as t};
+var a = {
+    email: "mohammedbasha9191@gmail.com",
+    phone: "+201115793403",
+    whatsapp: "https://wa.me/201115793403",
+    linkedin: "https://linkedin.com/in/mohammed-basha",
+    github: "https://github.com/mohammedbasha",
+};
+export { a as t };
